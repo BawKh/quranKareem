7365035ec241c90c45de17051152c616cdb690d7
BawKh <baselwael2222@gmail.com>
2024-10-15 19:13:25 +0300

    Initial commit

--- src/components/Reciters.js
+++ src/components/Reciters.js
@@ -1,15 +1,14 @@
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/esm/Button";
+
 import Col from "react-bootstrap/esm/Col";
 import { useEffect, useState } from "react";
 import img1 from "../images/Mishary Rashid Al-Afasy.jpg";
 import img2 from "../images/Abu Bakr Al-Shatri.jpg";
-import { Link } from "react-router-dom";
+
 function Reciters() {
   let [Reciters, setReciters] = useState([]);
-  const [RecNum, setRecNum] = useState(1);
   let Images = [img1, img2];
   const getReciters = () => {
     fetch("https://quranapi.pages.dev/api/reciters.json")
