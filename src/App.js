f77191e407ae3b8d26a0c7544a5d04f1da9e4ee9
BawKh <baselwael2222@gmail.com>
2024-10-15 18:22:30 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/MyNavbar";
+import Home from "./components/Home";
+import AppFooter from "./components/Footer";
+import Moshaph from "./components/Moshaph";
+import Surahs from "./components/Surah";
+import GoToTopButton from "./components/GoToTop";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/moshaph" element={<Moshaph />}></Route>
+        <Route path="/moshaph/:surahID" element={<Surahs />}></Route>
+      </Routes>
+      <GoToTopButton />
+      <AppFooter />
+    </>
   );
 }
 
